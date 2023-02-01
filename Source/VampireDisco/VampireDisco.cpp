786eba3f83bf2a19541adc49cd1bbe5c532bd782
Hrishikesh <hrishikeshpramodnair@gmail.com>
2023-02-01 20:09:59 +0530

    Created grid, player, object pick/drop

---
+++ Source/VampireDisco/VampireDisco.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VampireDisco.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VampireDisco, "VampireDisco" );
